6d08a6a619b70e5f09ed6e7bab8da9d37f69a95f
daltonoswald <daltonoswald@gmail.com>
2024-12-12 12:02:49 -0500

    trying spotify-web-api-js package

--- spotify/frontend/src/App.jsx
+++ spotify/frontend/src/App.jsx
@@ -1,11 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { loginEndpoint } from './Spotify'
 import axios from 'axios'
 import Playback from './components/Playback';
 import TopSongs from './components/TopSongs'
 import TopArtists from './components/TopArtists';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
+
+// const getTokenFromUrl = () => {
+//   return window.location.hash.substring(1).split("&").reduce((initial, item) => {
+//     let parts = item.split('=');
+//     initial[parts[0]] = decodeURIComponent(parts[1]);
+//     console.log('intial: ', initial[parts[0]])
+//     return initial;
+//   }, {});
+// }
 
 function App() {
+  const [spotifyToken, setSpotifyToken] = useState('');
+  const [nowPlaying, setNowPlaying] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    const queryParamaters = new URLSearchParams(window.location.search)
+    const access_token = queryParamaters.get('access_token')
+    const spotifyToken = access_token
+    console.log(queryParamaters);
+    // window.location.search = '';
+    // queryParamaters.delete('access_token');
+
+    if (spotifyToken) {
+      setSpotifyToken(spotifyToken);
+      spotifyApi.setAccessToken(spotifyToken)
+      spotifyApi.getMe().then((user) => {
+        console.log(user);
+        setProfile(user)
+      })
+      setLoggedIn(true);
+    }
+  },[])
+
+  const getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log(response);
+      setNowPlaying({
+        name: response.item.name,
+        albumArt: response.item.album.images[0].url
+      })
+    })
+  }
   
   const handleGetSavedAlbums = () => {
     const queryParamaters = new URLSearchParams(window.location.search)
@@ -23,7 +68,24 @@ function App() {
 
   return (
     <>
-      <a href={loginEndpoint}>Log In</a>
+      {!loggedIn && (
+        <a href={loginEndpoint}>Log In</a>
+      )}
+      {loggedIn && (
+        <>
+          <div className='profile'>
+            <img src={profile.images[0].url} style={{height: 150}} />
+            <div>{profile.display_name}</div>
+          </div>
+          <div>Now Playing: {nowPlaying.name}</div>
+          <div>
+            <img src={nowPlaying.albumArt} style={{height: 150}} />
+          </div>
+        </>
+      )}
+      {loggedIn && (
+        <button onClick={() => getNowPlaying()}>Check Now Playing</button>
+      )}
       <button onClick={handleGetSavedAlbums}>Saved Albums</button>
       <TopArtists />
       <TopSongs />
